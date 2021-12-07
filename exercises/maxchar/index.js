2b6923bc0ac2aadefce7a8f020da8f0f3260936f
AjayChandraOfficial <ajaychandraofficial@gmail.com>
2021-12-07 21:26:40 +0530

    Added algorithms

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,22 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  const chars = {};
+  for (const char of str) {
+    chars[char] = chars[char] ? chars[char] + 1 : 1;
+  }
+
+  let max = 0;
+  let maxChar = "";
+  for (const char in chars) {
+    if (chars[char] > max) {
+      max = chars[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
