f47610a4e20a572afed4df2789d1149e77452cb0
AjayChandraOfficial <ajaychandraofficial@gmail.com>
2021-12-08 21:58:06 +0530

    Anagram solved

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -7,7 +7,43 @@
 //   anagrams('rail safety', 'fairy tales') --> True
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
-
-function anagrams(stringA, stringB) {}
-
+const anagrams = (stringA, stringB) =>
+  refineString(stringA) === refineString(stringB);
+const refineString = (str) =>
+  str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
 module.exports = anagrams;
+
+// Slution1;
+// function anagrams(stringA, stringB) {
+//     const stringA_shrinked = stringA
+//       .replace(/[^\w]/g, "")
+//       .toLowerCase()
+//       .split("");
+//     const stringB_shrinked = stringB
+//       .replace(/[^\w]/g, "")
+//       .toLowerCase()
+//       .split("");
+
+//     if (stringA_shrinked.length !== stringB_shrinked.length) return false;
+
+//     const makeCharacterMap = (arr) => {
+//       const strChars = {};
+//       for (const char of arr) {
+//         if (strChars[char]) {
+//           strChars[char]++;
+//         } else {
+//           strChars[char] = 1;
+//         }
+//       }
+//       return strChars;
+//     };
+
+//     const strA_Map = makeCharacterMap(stringA_shrinked);
+//     const strB_Map = makeCharacterMap(stringB_shrinked);
+
+//     let isEqual = true;
+//     for (const char in strA_Map) {
+//       if (strA_Map[char] !== strB_Map[char]) isEqual = false;
+//     }
+//     return isEqual;
+//   }
